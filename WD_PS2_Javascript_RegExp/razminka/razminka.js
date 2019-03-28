1d0987006f741a3bd5d4c260ff24e5ce53f4f8d4
WolfEye123 <30938894+WolfEye123@users.noreply.github.com>
2019-03-28 15:26:27 +0200

    WD_PS2_JavaScript_RegExp razminka ex #1, #2, #3, #4 complete

--- WD_PS2_Javascript_RegExp/razminka/razminka.js
+++ WD_PS2_Javascript_RegExp/razminka/razminka.js
@@ -20,6 +20,10 @@ function calculateNumbers() {
 function calculateFromSeconds() {
     let sNum = +document.getElementById("timeInput1").value;
     const answer = document.getElementsByClassName("answerEx2")[0];
+    if (sNum < 0){
+        answer.innerHTML = "Incorrect number. Please try again.";
+        return;
+    }
     let mNum = Math.floor((sNum - (sNum % 60)) / 60 % 60);
     let hNum = Math.floor(sNum / 60 / 60);
     sNum = sNum % 60;
@@ -30,6 +34,10 @@ function calculateToSeconds() {
     let time = document.getElementById("timeInput2").value;
     const answer = document.getElementsByClassName("answerEx2_1")[0];
     let sec = time.split(':', 3);
+    if (sec[0] < 0 || sec[1] < 0 || sec[2] < 0) {
+        answer.innerHTML = "Incorrect time. Please try again.";
+        return;
+    }
     let seconds = sec[0] * 3600;
     seconds += sec[1] * 60;
     seconds += +sec[2];
@@ -40,14 +48,50 @@ function calculateTimeBetween() {
     const ftime = document.getElementById("timeInput3").value;
     const stime = document.getElementById("timeInput4").value;
     const answer = document.getElementsByClassName("answerEx3")[0];
-    let date1 = ftime.split('T',2);
-    let time1 = date1[1].split(':',2);
-    date1 = date1[0].split('-',3);
-    let date2 = stime.split('T',2);
-    let time2 = date2[1].split(':',2);
-    date2 = date2[0].split('-',3);
+    let date1 = ftime.split('T', 2);
+    let time1 = date1[1].split(':', 2);
+    date1 = date1[0].split('-', 3);
+    let date2 = stime.split('T', 2);
+    let time2 = date2[1].split(':', 2);
+    date2 = date2[0].split('-', 3);
     answer.innerHTML = (date2[0] - date1[0]) + " year (s), " + (date2[1] - date1[1]) + " month (s), " +
-        (date2[2] - date1[2]) + " day (s), " +   (time2[0] - time1[0]) + " hour (s), " + (time2[1] - time1[1]) +
+        (date2[2] - date1[2]) + " day (s), " + (time2[0] - time1[0]) + " hour (s), " + (time2[1] - time1[1]) +
         " minute (s).";
 }
 
+function drawChessBoard() {
+    let boardSize = document.getElementById("chess").value;
+    const answer = document.getElementsByClassName("answerEx4_1")[0];
+    const answerErr = document.getElementsByClassName("answerEx4")[0];
+    answer.innerHTML = "";
+    boardSize = boardSize.split('x', 2);
+    let block;
+    let br;
+    let x = boardSize[0];
+    let y = boardSize[1];
+    if (x < 0 || y < 0) {
+        answerErr.innerHTML = "Incorrect size. Please try again.";
+        return;
+    }
+    answer.style.width = x * 50; // size in "px"
+    answer.style.height = y * 50; // size in "px"
+    let flag = true;
+    for (let i = 0; i < y; i++) {
+        br = document.createElement("br");
+        for (let j = 0; j < x; j++) {
+            block = document.createElement("div");
+            if (flag) {
+                block.className = 'block black';
+            } else {
+                block.className = 'block white';
+            }
+            answer.appendChild(block);
+            flag = !flag;
+            if (j == x - 1 && x % 2 != 0){
+                flag = !flag;
+            }
+        }
+        flag = !flag;
+        answer.append(br);
+    }
+}
