2899b477a10a23141ba7b5b996cb981294995b18
WolfEye123 <30938894+WolfEye123@users.noreply.github.com>
2019-04-02 15:28:37 +0300

    WD_PS3_jQuery

--- WD_PS3_jQuery/script.js
+++ WD_PS3_jQuery/script.js
@@ -6,20 +6,33 @@ const NAMES = [
     "Matt"
 ];
 
-// const div = document.createElement('div.wrap');
 const ul = "<ul class='select'></ul>";
 const li = "<li></li>";
+let flag = true;
 
 $(document).ready(() => {
     $('.newSelector').append(ul);
-    $('.select').append($(li).addClass('fOption').html(`<label id="sel">Select Friend<span>▼</span></label>`));
+    $('.select').append($(li).addClass('fOption').html(`<label>Select Friend</label><span>▼</span>`));
     for (let name of NAMES) {
         $('.select').append($(li).addClass('hide').html(`<img src="images/${name}.png" alt="${name}"><label>${name}</label>`));
     }
+    $('li:last').css('border','none');
 
 
-    $('#sel').click(function () {
+    $('li.fOption').click(function () {
+        if (flag === false) {
+            flag = !flag;
+            $('li.fOption').html(`<label>Select Friend</label><span>▼</span>`);
+        }
         $('li').toggleClass('option');
+    })
 
+    $('li.hide').click(function () {
+        $('li').toggleClass('option');
+        let name = $(this).text();
+        if (flag) {
+            $('li.fOption').html(`<img src="images/${name}.png" alt="${name}"><label>${name}</label><span>▼</span>`);
+            flag = !flag;
+        }
     })
 });
