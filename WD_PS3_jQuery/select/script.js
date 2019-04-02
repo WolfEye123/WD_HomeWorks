ead9c45f664020147eecd101065ce39f4933b58c
WolfEye123 <30938894+WolfEye123@users.noreply.github.com>
2019-04-02 19:04:37 +0300

    WD_PS3_jQuery refactor code

--- WD_PS3_jQuery/select/script.js
+++ WD_PS3_jQuery/select/script.js
@@ -6,24 +6,36 @@ const NAMES = [
     "Matt"
 ];
 
-const ul = "<ul class='select'></ul>";
-const li = "<li></li>";
-let flag = true;
-const select = $('.select');
-const newSelector = $('.newSelector');
-const liHide = $('li.hide');
-const liFOption = $('li.fOption');
-const liLast = $('li:last');
-
 $(document).ready(() => {
+    let flag = true;
+
+    // html objects
+    const ul = "<ul class='select'></ul>";
+    const li = "<li></li>";
+
+    //section selector
+    const newSelector = $('.newSelector');
+
+    //create list
     newSelector.append(ul);
+
+    // select selector
+    const select = $('.select');
+
+    // create list objects
     select.append($(li).addClass('fOption').html(`<label>Select Friend</label><span>▼</span>`));
     for (let name of NAMES) {
         select.append($(li).addClass('hide').html(`<img src="images/${name}.png" alt="${name}"><label>${name}</label>`));
     }
 
+    // list selectors
+    const liFOption = $('li.fOption');
+    const liLast = $('li:last');
+    const liHide = $('li.hide');
+
     liLast.css('border','none');
 
+    // option hover
     liHide.mouseenter(function () {
         $(this).css('background-color', 'aliceblue');
     });
@@ -31,14 +43,14 @@ $(document).ready(() => {
        $(this).css('background-color', 'white');
     });
 
-
+    // EventListeners
     liFOption.click(function () {
         if (flag === false) {
             flag = !flag;
             liFOption.html(`<label>Select Friend</label><span>▼</span>`);
         }
         $('li').toggleClass('option');
-    })
+    });
 
     liHide.click(function () {
         $('li').toggleClass('option');
